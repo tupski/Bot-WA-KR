302653989f72e9c974f1e310135cd3a3c219fe95
Angga Artupas <artupski@gmail.com>
2025-08-07 04:25:33 +0700

    Fix: Tambahkan require moment-timezone di fungsi parseMonthName untuk mengatasi ReferenceError

--- index.js
+++ index.js
@@ -2202,6 +2202,7 @@ function isMonthName(param) {
  * Parse month name to month number and year
  */
 function parseMonthName(param) {
+    const moment = require('moment-timezone');
     const monthNames = {
         'januari': 1, 'februari': 2, 'maret': 3, 'april': 4, 'mei': 5, 'juni': 6,
         'juli': 7, 'agustus': 8, 'september': 9, 'oktober': 10, 'november': 11, 'desember': 12
